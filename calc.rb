88935e68716a5d67f361e56f45bd6dad8eb05192
fishermanswharff <fishermanswharff@github.com>
2014-09-23 21:56:07 -0400

    calculator v.1.0, basic operations

--- calc.rb
+++ calc.rb
@@ -1,1 +1,54 @@
-# Your code goes here
+# calculator v.1.0, only takes simple arithmetic in the form of (fixnum +/-/*/÷ fixnum)
+def process_calc(string)
+  numbers = string.scan(/[\d]+/)
+  operators = string.scan(/[\+\-\*\/]/)
+  groups = string.scan(/([\(]+)([\d]+)([\+\-\*\/]+)([\d]+)([\)]+)/)
+  set_calc(numbers,operators,groups)
+end
+
+def set_calc(numbers,operators,groups)
+  result = operators.map { |i| numbers[0].to_i.method(i).(numbers[1].to_i) }
+  puts "The answer is #{result[0]}"
+end
+
+puts "Calculator can add, subtract, multiply, and divide. Try it"
+process_calc(gets.chomp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
